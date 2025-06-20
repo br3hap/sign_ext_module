fb2d1e7f6a2adb65c4bb9c7090ae3f0f3b7d59af
Breithner <Breithnerap@gmail.com>
2025-06-19 21:28:27 -0500

    upload change

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sign Ext options',
+    'version': '1.0',
+    'summary': """ Módulo que extiende el módulo sign, agrega eventos en el wizard del firmante - odoo 18 """,
+    'author': 'Breithner Aquituari',
+    'website': '',
+    'category': '',
+    'depends': ['base', 'sign'],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
